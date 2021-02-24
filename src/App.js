51650ea210eac499cbdbe9b8360d2cced20e86ea
themockingjet <jwoot001@gmail.com>
2021-02-24 16:23:46 +0800

    dropdown fix

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import './assets/css/bootstrap.min.css';
-import './assets/css/style.min.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import React from "react";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/style.min.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
+import "bootstrap/js/dist/dropdown";
 
-import { NavHeader, Jumbotron, Features, Testimonies,
-          Portfolio, Collaborate, Contact, Cta, Footer} from './components/index';
+import {
+  NavHeader,
+  Jumbotron,
+  Features,
+  Testimonies,
+  Portfolio,
+  Collaborate,
+  Contact,
+  Cta,
+  Footer,
+} from "./components/index";
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
-  componentDidMount(){
+  componentDidMount() {
     AOS.init({
-      duration : 2000
-    })
+      duration: 2000,
+    });
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <NavHeader/>
-        <Jumbotron/>
-        <Features/>
-        <Testimonies/>
+        <NavHeader />
+        <Jumbotron />
+        <Features />
+        <Testimonies />
         {/* <Portfolio/> */}
-        <Collaborate/>
-        <Contact/>
-        <Cta/>
-        <Footer/>
+        <Collaborate />
+        <Contact />
+        <Cta />
+        <Footer />
       </div>
-
-    
-    )
+    );
   }
-};
+}
 
 export default App;
--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React from "react";
 
 function NavHeader() {
-    return (
-        <nav id="header-navbar" className="navbar navbar-expand-lg py-4">
-            <div className="container">
-                <a className="navbar-brand d-flex align-items-center text-white" href="/">
-                    <h3 className="font-weight-bolder mb-0"><i className="fas fa-star-and-crescent"></i></h3>
-                </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-nav-header" aria-controls="navbar-nav-header" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="lnr lnr-menu"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbar-nav-header">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#features">Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#portfolio">Images</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#client">Collaborate</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#reservation">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav id="header-navbar" className="navbar navbar-expand-lg py-4">
+      <div className="container">
+        <a
+          className="navbar-brand d-flex align-items-center text-white"
+          href="/"
+        >
+          <h3 className="font-weight-bolder mb-0">
+            <i className="fas fa-star-and-crescent"></i>
+          </h3>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbar-nav-header"
+          aria-controls="navbar-nav-header"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="lnr lnr-menu"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbar-nav-header">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#features">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#portfolio">
+                Images
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#client">
+                Collaborate
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#reservation">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default NavHeader
+export default NavHeader;
